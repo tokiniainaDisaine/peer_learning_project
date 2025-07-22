e4decaf4bb1bddc02f8e308ba9d59d576776828c
tokiniainaDisaine <t.disaine@alustudent.com>
2025-07-22 15:31:07 +0200

    init commit

---
+++ micro_save.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Welcome to MicroSave :)
+
+The app to help you reach your financial goals
+"""
+
+
+# User input here
+
+"""
+For the user input,
+
+We'll need to get those variables:
+
+Main expenses:
+-income
+-food expense
+-rent expense
+-transportation
+
+Optional one:
+Asks the user for the expense name
+Then asks how much does it cost
+"""
+
+
+
+# Calculate the present income/expense
+
+
+# Calculate the present income/expense
+
+
